67ff84b9ff58643a5bcb9c2cbe900e11f514fc72
DRFR34 <125657204+DRFR34@users.noreply.github.com>
2024-04-24 14:20:24 +0200

    Create CommunityPage.jsx

---
+++ frontend/src/pages/CommunityPage/CommunityPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import UnderConstruction from '../../components/UnderConstruction/UnderConstruction';
+
+export default function CommunityPage() {
+  return (
+    < >
+
+      <UnderConstruction margin="12.5rem auto" />
+
+    </>
+  )
+}
