c12a9a2a3e6903d70e4ff157c70df56e476c0a19
DRFR34 <125657204+DRFR34@users.noreply.github.com>
2024-04-26 00:15:03 +0200

    Update UnderConstruction.jsx

--- frontend/src/components/UnderConstruction/UnderConstruction.jsx
+++ frontend/src/components/UnderConstruction/UnderConstruction.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
 import './UnderConstruction.scss'
 
+
+/**
+ * @description UnderConstruction component displays a message indicating that the page is under construction.
+ *
+ * @param {Object} props - The props of the UnderConstruction component.
+ * @param {string} props.margin - The margin style for the container.
+ * @returns {React.Element} UnderConstruction component
+ */
 export default function UnderConstruction({margin}) {
   return (
     <div className='underConstContainer' style={{ margin: margin }} >
@@ -11,3 +20,7 @@ export default function UnderConstruction({margin}) {
     </div>
   )
 }
+
+UnderConstruction.propTypes = {
+  margin: PropTypes.string.isRequired
+};
