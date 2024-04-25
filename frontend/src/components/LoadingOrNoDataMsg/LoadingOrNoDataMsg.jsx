a2a5f63ba89fc29a1e7b36651ad950d38c3d3d73
DRFR34 <125657204+DRFR34@users.noreply.github.com>
2024-04-25 17:13:32 +0200

    Update LoadingOrNoDataMsg.jsx

--- frontend/src/components/LoadingOrNoDataMsg/LoadingOrNoDataMsg.jsx
+++ frontend/src/components/LoadingOrNoDataMsg/LoadingOrNoDataMsg.jsx
@@ -1,21 +1,26 @@
 import React from 'react'
-
+import PropTypes from 'prop-types';
 import "./LoadingOrNoDataMsg.scss"
 
-export default function LoadingOrNoDataMsg({isLoading, expectedData}) {
-  
+export default function LoadingOrNoDataMsg({ isLoading, expectedData }) {
+
   if (isLoading) {
     return (
       <div className='LoadingOrNoDataMsg'>
         {isLoading && <div className="loading-message">⏳ Chargement en cours...</div>}
-        </div>
-        )
+      </div>
+    )
   }
-  
+
   return (
     <div className='LoadingOrNoDataMsg'>
       {isLoading && <div className="loading-message">Chargement en cours...</div>}
-      {!expectedData  && <div className="error-message">Aucune donnée n'est disponible pour l'utilisateur spécifié.</div>}
+      {!expectedData && <div className="error-message">Aucune donnée n'est disponible pour l'utilisateur spécifié.</div>}
     </div>
   )
 }
+
+LoadingOrNoDataMsg.propTypes = {
+  isLoading: PropTypes.bool.isRequired,
+  expectedData: PropTypes.object
+};
