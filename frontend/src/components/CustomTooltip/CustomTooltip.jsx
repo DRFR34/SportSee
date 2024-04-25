94f117633c435ae3c9d0437c4492efc40f05b189
DRFR34 <125657204+DRFR34@users.noreply.github.com>
2024-04-25 19:30:31 +0200

    Update CustomTooltip.jsx

--- frontend/src/components/CustomTooltip/CustomTooltip.jsx
+++ frontend/src/components/CustomTooltip/CustomTooltip.jsx
@@ -1,12 +1,24 @@
 
 
 import React from 'react'
+import PropTypes from 'prop-types';
 
-export default function CustomTooltip({ active, payload, label, boxClass }) {
+
+/**
+ * CustomTooltip component displays a customized tooltip for a Recharts chart.
+ *
+ * @param {Object} props - Component props
+ * @param {boolean} props.active - Whether the tooltip is active or not
+ * @param {Object[]} props.payload - Array of data points to display in the tooltip
+ * @param {string} props.label - Label for the tooltip
+ * @param {string} props.boxClass - CSS class for the tooltip box
+ * @returns {React.Element | null} CustomTooltip component
+ */
+export default function CustomTooltip({ active, payload, customTooltipClass }) {
     if (active && payload && payload.length) {
 
         return (
-            <div className={`${boxClass}`}>
+            <div className={`${customTooltipClass}`}>
               <span className="customTooltip__item">{ `${payload[0].value} kg`}</span>
               <span className="customTooltip__item">{ `${payload[1].value} kCal`}</span>
             </div>
@@ -15,3 +27,14 @@ export default function CustomTooltip({ active, payload, label, boxClass }) {
     return null;
 }
  
+
+CustomTooltip.propTypes = {
+  active: PropTypes.bool,
+  payload: PropTypes.arrayOf(
+    PropTypes.shape({
+      value: PropTypes.number.isRequired,
+    })
+  ),
+  label: PropTypes.string,
+  customTooltipClass: PropTypes.string.isRequired,
+};
