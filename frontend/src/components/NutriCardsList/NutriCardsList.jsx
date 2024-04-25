70c985444d0b7932daf4676f5fc8f69e1e813ad1
DRFR34 <125657204+DRFR34@users.noreply.github.com>
2024-04-25 19:00:33 +0200

    Update NutriCardsList.jsx

--- frontend/src/components/NutriCardsList/NutriCardsList.jsx
+++ frontend/src/components/NutriCardsList/NutriCardsList.jsx
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
 
 import NutritionCard from '../NutritionCard/NutritionCard';
-
 import ApiService from '../../utils/apiService';
 import LoadingOrNoDataMsg from '../LoadingOrNoDataMsg/LoadingOrNoDataMsg';
+
 import fatIcon from "../../assets/images/fat-icon.png";
 import carbsIcon from "../../assets/images/carbs-icon.png";
 import proteinIcon from "../../assets/images/protein-icon.png";
@@ -12,15 +13,21 @@ import caloriesIcon from "../../assets/images/calories-icon.png";
 
 import "./NutriCardsList.scss"
 
-
-
+/**
+ * NutriCardsList component displays a list of nutrition cards.
+ * It fetches the main user data and generates NutritionCard components based on the data.
+ *
+ * @param {Object} props - Component props
+ * @param {number} props.currentUserId - ID of the current user
+ * @returns {JSX.Element} NutriCardsList component
+ */
 export default function NutriCardsList({ currentUserId }) {
 
     const [userMainData, setUserMainData] = useState(null); //userMainData is object
     const [isLoading, setIsLoading] = useState(false);
 
     useEffect(() => {
-        
+
         const getUserMainData = async () => {
             setIsLoading(true);
             const fetchedData = await ApiService.getUserMainData(currentUserId);
@@ -78,3 +85,8 @@ export default function NutriCardsList({ currentUserId }) {
     )
 
 }
+
+
+NutriCardsList.propTypes = {
+    currentUserId: PropTypes.number.isRequired,
+  };
