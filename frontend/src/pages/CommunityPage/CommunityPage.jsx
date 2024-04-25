48a735d6755526e8a9f16339eb4310b5105294ca
DRFR34 <125657204+DRFR34@users.noreply.github.com>
2024-04-26 00:13:53 +0200

    Update CommunityPage.jsx

--- frontend/src/pages/CommunityPage/CommunityPage.jsx
+++ frontend/src/pages/CommunityPage/CommunityPage.jsx
@@ -2,6 +2,11 @@ import React from 'react'
 
 import UnderConstruction from '../../components/UnderConstruction/UnderConstruction';
 
+/**
+ * @description CommunityPage component renders the Community page content. (currently under construction.)
+ *
+ * @returns {JSX.Element} CommunityPage component
+ */
 export default function CommunityPage() {
   return (
     < >
