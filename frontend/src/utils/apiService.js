f5ee430d410590395e757b9906ec04cf4f311f10
DRFR34 <125657204+DRFR34@users.noreply.github.com>
2024-04-25 17:09:38 +0200

    Update apiService.js

--- frontend/src/utils/apiService.js
+++ frontend/src/utils/apiService.js
@@ -1,5 +1,17 @@
 
 
+
+  // The SportSee API includes four kinds of endpoints:
+  // `http://localhost:3000/user/${userId}` 
+  // `http://localhost:3000/user/${userId}/activity` 
+  // `http://localhost:3000/user/${userId}/average-sessions` 
+  // `http://localhost:3000/user/${userId}/performance` 
+ 
+
+
+
+
+
 import axios from 'axios';
 import UserModelisedData from './dataModelisationClass';
 import {
@@ -10,23 +22,27 @@ import {
 } from '../assets/data/mockedData';
 
 
-
-/** The SportSee API includes four kinds of endpoints: 
- * 
-
-- `http://localhost:3000/user/${userId}` 
-- `http://localhost:3000/user/${userId}/activity` 
-- `http://localhost:3000/user/${userId}/average-sessions` 
-- `http://localhost:3000/user/${userId}/performance` 
+/**
+ * Create a new instance of Axios with a custom basURL.
+ * the 3005 port is fictiv and it is used to test the failling of the server response
+ * @type {import("axios").AxiosInstance}
  */
-
-
 const axiosAPI = axios.create({
   baseURL: 'http://localhost:3000/user/'
   // baseURL: 'http://localhost:3005/user/'
 });
 
+const noServerMsg = `
+    **************** ATTENTION ******************** 
+
+            Le serveur n'a pas répondu.
+    Les graphiques afficheront vos données locales.
+
+    ***********************************************
+`;
+
 /**
+ * API service for handling user-related data.
  * The ApiService class owns static methods.
  * Each of these methods serves to fetch the data from one of the API end-points.
  * If the loading of the distant data fails, the method substitute it with the corresponding local data.
@@ -39,46 +55,90 @@ const axiosAPI = axios.create({
  */
 export default class ApiService {
 
+  static alertShown = false; 
+
+  /**
+   * In case the server is not responding, fetch the local data, and displays an alert informing the user.
+   * @static
+   * @param {number} currentUserId - The ID of the current user
+   * @param {string} idKey - The key used for user ID in the data
+   * @param {Array} relevantLocalData - Array of relevant local data objects
+   * @returns {object} - User data object
+   */
+  static handleServerError(currentUserId, idKey, relevantLocalData) {
+
+    if (!ApiService.alertShown) {
+      alert(noServerMsg);
+      ApiService.alertShown = true;
+    }
+
+    const currentUserData = relevantLocalData.find((usersData) => usersData[idKey] === currentUserId);
+     
+    if (currentUserData) {
+      return new UserModelisedData(currentUserData);
+    } 
+    else {
+      throw new Error('User not found');
+    }
+  }
+
+  /**
+   * Retrieves the main data for a user (online, else local).
+   * @static
+   * @param {number} currentUserId - The ID of the current user
+   * @returns {object} - User main data object
+   */
   static async getUserMainData(currentUserId) {
     try {
       const response = await axiosAPI.get(`${currentUserId}`);
       return new UserModelisedData(response.data.data);
     } catch (Error) {
-      console.log("new UserModelisedData(USER_MAIN_DATA.find((userMainData) => userMainData.id === currentUserId))", new UserModelisedData(USER_MAIN_DATA.find((userMainData) => userMainData.id === currentUserId)));
-
-      // return USER_MAIN_DATA.find((userMainData) => userMainData.id === currentUserId);
-      const currentUserMainData = USER_MAIN_DATA.find((userMainData) => userMainData.id === currentUserId)
-      return new UserModelisedData(currentUserMainData);
-
+      return ApiService.handleServerError(currentUserId, "id", USER_MAIN_DATA)
     };
   }
 
+  /**
+   * Retrieves the activity data for a user (online, if not local).
+   * @static
+   * @param {number} currentUserId - The ID of the current user
+   * @returns {object} - User activity data object
+   */
   static async getUserActivity(currentUserId) {
     try {
       const response = await axiosAPI.get(`${currentUserId}/activity`);
       return new UserModelisedData(response.data.data);
     } catch (Error) {
-       return USER_ACTIVITY.find((useractivity) => useractivity.userId === currentUserId);
+      return ApiService.handleServerError(currentUserId, "userId", USER_ACTIVITY)
     }
   }
 
-
-
+ /**
+   * Retrieves the average sessions data for a user (online, else, local)
+   * @static
+   * @param {number} currentUserId - The ID of the current user
+   * @returns {object} - User average sessions data object
+   */
   static async getUserAverageSessions(currentUserId) {
     try {
       const response = await axiosAPI.get(`${currentUserId}/average-sessions`);
       return new UserModelisedData(response.data.data);
     } catch (Error) {
-      return USER_AVERAGE_SESSIONS.find((userAverageSessions) => userAverageSessions.userId === currentUserId);
+      return ApiService.handleServerError(currentUserId, "userId", USER_AVERAGE_SESSIONS)
     }
   }
 
+/**
+   * Retrieves the performance data for a user (online, else local)
+   * @static
+   * @param {number} currentUserId - The ID of the current user
+   * @returns {object} - User performance data object
+   */
   static async getUserPerformance(currentUserId) {
     try {
       const response = await axiosAPI.get(`${currentUserId}/performance`);
       return new UserModelisedData(response.data.data);
     } catch (Error) {
-      return USER_PERFORMANCE.find((userPerformance) => userPerformance.userId === currentUserId);
+      return ApiService.handleServerError(currentUserId, "userId", USER_PERFORMANCE)
     }
   }
 }
