d9b1bd22a87b111d29cc148484ebc59cab38790e
DRFR34 <125657204+DRFR34@users.noreply.github.com>
2024-04-29 14:07:00 +0200

    Update App.jsx

--- frontend/src/app/App.jsx
+++ frontend/src/app/App.jsx
@@ -1,32 +1,35 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import TopBar from '../components/TopBar/TopBar';
 import SideBar from '../components/SideBar/SideBar';
-import HomePage from '../pages/HomePage/HomePage.jsx';
-import ProfilePage from '../pages/ProfilePage/ProfilePage.jsx';
-import SettingsPage from '../pages/SettingsPage/SettingsPage.jsx';
-import CommunityPage from '../pages/CommunityPage/CommunityPage.jsx';
-import Error404Page from '../pages/Error404Page/Error404Page.jsx';
+import SiteRoutes from '../components/SiteRoutes/SiteRoutes.jsx';
+
 
 import "./App.scss"
 
-
+/**
+ * The main app component.
+ * @export {JSX.Element.Component}
+ * @returns {*}
+ */
 export default function App() {
   return (
     <React.StrictMode>
-      <BrowserRouter basenam="/sportsee">
+      <BrowserRouter
+        basename="/sportsee"
+      >
         <TopBar />
-        <div className='SideBarAndMainContainer'>
+        <div
+          className='SideBarAndMainContainer'
+        >
           <SideBar />
-          <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/profil/:idSlug"  element={<ProfilePage />} />
-            <Route path="/reglage" element={<SettingsPage />} />
-            <Route path="/communaute" element={<CommunityPage />} />
-            <Route path="*" element={<Error404Page />} />
-          </Routes>
+
+          <SiteRoutes />
+
         </div>
+
       </BrowserRouter>
+
     </React.StrictMode>
   )
 }
