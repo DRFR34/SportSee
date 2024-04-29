7c500464ee1e8d2c3c8fd25c4572967ba1b5eaf8
DRFR34 <125657204+DRFR34@users.noreply.github.com>
2024-04-29 12:06:05 +0200

    Update Error404Page.jsx

--- frontend/src/pages/Error404Page/Error404Page.jsx
+++ frontend/src/pages/Error404Page/Error404Page.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
 
 import './Error404Page.scss'
 
-export default function error404Page({errorText='😕 La ressource demandée est introuvable.', homeLinkText="Retouner à la page d'accueil."}) {
+/**
+ * Component for the error 404 page, is customizable following context, for a better user information.
+ * @param {Object} props - The props for the error 404 page.
+ * @param {string} props.errorText - The text for the error witth a default text
+ * @param {string} props.homeLinkText - The text for the home link with a default 
+ * @returns {JSX.Element} The error 404 page component.
+ */
+export default function error404Page({errorText='😕 La page demandée est introuvable.', homeLinkText="Retouner à la page d'accueil."}) {
   return (
     <main>
       <h1 className="errorCode">404</h1>
@@ -16,6 +24,7 @@ export default function error404Page({errorText='😕 La ressource demandée est
   )
 }
 
-//== Other contents ideas:
-//😕 Désolé, aucun utlisateur n'est sélectionné.
-//Retourner sur la page d’accueil pour sélectionner un utilisateur.
+error404Page.propTypes = {
+  errorText: PropTypes.string,
+  homeLinkText: PropTypes.string
+}
